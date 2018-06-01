22104944cc5c4f6acd358b786febb35b8beffb94
Felipe Ortega Casillas <felipeorcas@gmail.com>
2018-05-31 18:19:30 -0700

    Fab buttons fixed

--- app/src/main/java/com/felipe/findmycar/MainActivity.java
+++ app/src/main/java/com/felipe/findmycar/MainActivity.java
@@ -2,11 +2,13 @@ package com.felipe.findmycar;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -18,6 +20,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 
+import java.io.Console;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -46,52 +49,60 @@ public class MainActivity extends AppCompatActivity {
         FabRClockwise = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_clockwise);
         FabRAnticlockwise = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_anticlockwise);
 
-        main_fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (isOpen) {
-                    fab_gps.startAnimation(FabClose);
-                    fab_tracking.startAnimation(FabClose);
-                    main_fab.startAnimation(FabRClockwise);
-                    main_fab.setImageResource(R.drawable.ic_action_arrow_drop_up);
-                    fab_gps.setClickable(false);
-                    fab_tracking.setClickable(false);
-                    isOpen = false;
-
-                } else {
-                    fab_gps.startAnimation(FabOpen);
-                    fab_tracking.startAnimation(FabOpen);
-                    main_fab.startAnimation(FabRAnticlockwise);
-                    main_fab.setImageResource(R.drawable.ic_action_close);
-                    fab_gps.setClickable(true);
-                    fab_tracking.setClickable(true);
-                    isOpen = true;
-                }
-            }
-
-
-        });
-
-        fab_tracking.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                fab_return_tracking.setImageResource(R.drawable.ic_action_return_tracking);
-                main_fab.setEnabled(false);
-                fab_return_tracking.setClickable(true);
-            }
-        });
-
-        fab_gps.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                fab_return_tracking.setImageResource(R.drawable.ic_action_return_tracking_gps);
-                main_fab.setEnabled(false);
-                fab_return_tracking.setClickable(true);
-            }
-        });
-
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
 
         // mapFragment.
     }
+
+    public void onTrackingClick(View view){
+        closeFabButton();
+        fab_return_tracking.setImageResource(R.drawable.ic_action_return_tracking);
+        main_fab.setBackgroundColor(Color.parseColor("#343434"));
+        main_fab.setEnabled(false);
+        fab_return_tracking.setClickable(true);
+        fab_return_tracking.setVisibility(View.VISIBLE);
+    }
+
+    public void onTrackingGpsClick(View view){
+        closeFabButton();
+        fab_return_tracking.setImageResource(R.drawable.ic_action_return_tracking_gps);
+        main_fab.setBackgroundColor(Color.parseColor("#343434"));
+        main_fab.setEnabled(false);
+        fab_return_tracking.setClickable(true);
+        fab_return_tracking.setVisibility(View.VISIBLE);
+    }
+
+    public void onMainFabButtonClick(View view){
+        if (isOpen) {
+            closeFabButton();
+
+        } else {
+            openFabButton();
+        }
+    }
+    public void onReturnTrackingClick(View view){
+        main_fab.setBackgroundColor(Color.parseColor("#DC552C"));
+        main_fab.setEnabled(true);
+        fab_return_tracking.setVisibility(View.INVISIBLE);
+    }
+
+    public void closeFabButton(){
+        fab_gps.startAnimation(FabClose);
+        fab_tracking.startAnimation(FabClose);
+        main_fab.setImageResource(R.drawable.ic_action_arrow_drop_up);
+        fab_gps.setClickable(false);
+        fab_tracking.setClickable(false);
+        isOpen = false;
+    }
+
+    public void openFabButton(){
+        fab_gps.startAnimation(FabOpen);
+        fab_tracking.startAnimation(FabOpen);
+        main_fab.startAnimation(FabRAnticlockwise);
+        main_fab.setImageResource(R.drawable.ic_action_close);
+        fab_gps.setClickable(true);
+        fab_tracking.setClickable(true);
+        isOpen = true;
+    }
+
 }
